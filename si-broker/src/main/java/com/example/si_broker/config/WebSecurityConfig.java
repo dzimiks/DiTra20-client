94799e757b366fa85b9db7f4ffabf907cdfcfd31
dzimiks <vana997@gmail.com>
2020-05-02 14:29:30 +0200

    Set web security config

---
+++ si-broker/src/main/java/com/example/si_broker/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.si_broker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors()
+                .and()
+                .csrf()
+                .disable();
+    }
+}
--- si-broker/src/main/java/com/example/si_broker/controllers/v1/UserController.java
+++ si-broker/src/main/java/com/example/si_broker/controllers/v1/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin
 @RequestMapping(Constants.USERS_BASE_URL)
 public class UserController {
 
