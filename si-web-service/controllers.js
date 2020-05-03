76a99b456d0ea64ec2bfcbc483d8a2e114e01937
dzimiks <vana997@gmail.com>
2020-05-03 15:28:16 +0200

    Add INSERT request for tables route

--- si-web-service/controllers.js
+++ si-web-service/controllers.js
@@ -15,6 +15,12 @@ module.exports.tablesSelect = (req, res) => {
   });
 };
 
+// {
+//     "tableName": "TYPES_OF_INSTITUTIONS",
+//     "value": {
+//         "TIP_UST": "MX"
+//     }
+// }
 module.exports.tablesDelete = (req, res) => {
   const tableName = req.body.tableName || 'TYPES_OF_INSTITUTIONS';
   const value = req.body.value || { 'TIP_UST': 'MX' };
@@ -25,6 +31,41 @@ module.exports.tablesDelete = (req, res) => {
   }
 
   query = query.substring(0, query.length - 5);
+
+  res.locals.connection.query(query, (error, results) => {
+    console.log(`Running query: ${query}`);
+
+    if (error) {
+      throw error;
+    }
+
+    res.send(JSON.stringify({ response: results }));
+  });
+};
+
+// {
+//     "tableName": "TYPES_OF_INSTITUTIONS",
+//     "value": {
+//         "TIP_UST": "MX",
+//         "TIP_NAZIV": "Red Bull"
+//     }
+// }
+module.exports.tablesInsert = (req, res) => {
+  const tableName = req.body.tableName || 'TYPES_OF_INSTITUTIONS';
+  const value = req.body.value || { 'TIP_UST': 'MX', 'TIP_NAZIV': 'Red Bull' };
+
+  let columns = '(';
+  let values = '(';
+
+  for (const val of Object.keys(value)) {
+    columns += `${val},`;
+    values += `'${value[val]}',`;
+  }
+
+  columns = columns.substring(0, columns.length - 1) + ')';
+  values = values.substring(0, values.length - 1) + ')';
+
+  const query = `INSERT INTO ${tableName} ${columns} VALUES ${values}`;
 
   res.locals.connection.query(query, (error, results) => {
     console.log(`Running query: ${query}`);
--- si-web-service/routes.js
+++ si-web-service/routes.js
@@ -6,6 +6,7 @@ const mainController = require('./controllers');
 // GET
 router.get('/api/v1/tables/select', mainController.tablesSelect);
 router.get('/api/v1/tables/delete', mainController.tablesDelete);
+router.get('/api/v1/tables/insert', mainController.tablesInsert);
 
 // POST
 router.post('/api/v1/tables/select', (req, res) => {
@@ -13,6 +14,13 @@ router.post('/api/v1/tables/select', (req, res) => {
 });
 
 router.post('/api/v1/tables/delete', (req, res) => {
+  res.json({
+    tableName: req.body.tableName,
+    value: res.body
+  });
+});
+
+router.post('/api/v1/tables/insert', (req, res) => {
   res.json({
     tableName: req.body.tableName,
     value: res.body
