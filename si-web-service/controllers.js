8a0ff16b8005ee0924a3eef4c39b36290eb4b00c
dzimiks <vana997@gmail.com>
2020-05-03 15:16:51 +0200

    Add DELETE request for tables route

--- si-web-service/controllers.js
+++ si-web-service/controllers.js
@@ -1,26 +1,38 @@
-module.exports.test = (req, res) => {
-  res.json({
-    test: 'dzimiks'
-  });
-};
-
-module.exports.tables = (req, res) => {
+module.exports.tablesSelect = (req, res) => {
   // TYPES_OF_INSTITUTIONS
   // HIGH_EDUCATION_INSTITUTION
   const tableName = req.body.tableName || 'TYPES_OF_INSTITUTIONS';
   const query = `SELECT * FROM ${tableName}`;
 
   res.locals.connection.query(query, (error, results) => {
-    console.log(`Running ${query}...`);
+    console.log(`Running query: ${query}`);
 
     if (error) {
       throw error;
     }
 
-    res.send(JSON.stringify({
-      status: 200,
-      error: null,
-      response: results
-    }));
+    res.send(JSON.stringify({ response: results }));
+  });
+};
+
+module.exports.tablesDelete = (req, res) => {
+  const tableName = req.body.tableName || 'TYPES_OF_INSTITUTIONS';
+  const value = req.body.value || { 'TIP_UST': 'MX' };
+  let query = `DELETE FROM ${tableName} WHERE `;
+
+  for (const val of Object.keys(value)) {
+    query += `${val}='${value[val]}' AND `;
+  }
+
+  query = query.substring(0, query.length - 5);
+
+  res.locals.connection.query(query, (error, results) => {
+    console.log(`Running query: ${query}`);
+
+    if (error) {
+      throw error;
+    }
+
+    res.send(JSON.stringify({ response: results }));
   });
 };
--- si-web-service/routes.js
+++ si-web-service/routes.js
@@ -4,12 +4,19 @@ const router = express.Router();
 const mainController = require('./controllers');
 
 // GET
-router.get('/', mainController.test);
-router.get('/api/v1/tables', mainController.tables);
+router.get('/api/v1/tables/select', mainController.tablesSelect);
+router.get('/api/v1/tables/delete', mainController.tablesDelete);
 
 // POST
-router.post('/api/v1/tables', (req, res) => {
+router.post('/api/v1/tables/select', (req, res) => {
   res.json({ tableName: req.body.tableName });
+});
+
+router.post('/api/v1/tables/delete', (req, res) => {
+  res.json({
+    tableName: req.body.tableName,
+    value: res.body
+  });
 });
 
 module.exports = router;
